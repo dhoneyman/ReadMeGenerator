c3f91fe88de6cc0befa94540bfb3f4b69576af7e
Dylan Honeyman <dylan@dylans-iMac.local>
2021-12-03 10:02:52 -0600

    license function

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,6 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if(license === ''){
+        return;
+    } else if(license === 'Public Domain Dedication and License (PDDL)'){
+        return `[![License: ODbL](https://img.shields.io/badge/License-PDDL-brightgreen.svg)](https://opendatacommons.org/licenses/pddl/)`
+    } else if(license === 'MIT License'){
+        return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    } else if(license === 'Apache 2.0 License'){
+        return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+    } else if(license === 'Boost Software License 1.0'){
+        return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
+    }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -46,6 +58,11 @@ ${data.installation}
 #Collaborators
 
 - [${collaborator}](${collabGitHub})
+
+##License
+
+-${data.license}
+-${renderLicenseBadge(data.license)}
 `;
 }
 
--- index.js
+++ index.js
@@ -1,94 +1,98 @@
 // TODO: Include packages needed for this application
 
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = 
-inquirer
-.prompt([
+const questions = inquirer
+  .prompt([
     {
-        type: 'input',
-        message: 'What is the title of your project?',
-        name: 'projectTitle',
+      type: "input",
+      message: "What is the title of your project?",
+      name: "projectTitle",
     },
     {
-        type: 'input',
-        message: 'Please provide your motivations for this project',
-        name: 'motivations',
+      type: "input",
+      message: "Please provide your motivations for this project",
+      name: "motivations",
     },
     {
-        type: 'input',
-        message: 'Why did you build this project?',
-        name: 'whyBuilt',
+      type: "input",
+      message: "Why did you build this project?",
+      name: "whyBuilt",
     },
     {
-        type: 'input',
-        message: 'What problem does this project solve?',
-        name: 'problem',
+      type: "input",
+      message: "What problem does this project solve?",
+      name: "problem",
     },
     {
-        type: 'input',
-        message: 'What leasons did you learn in the process of completing this project?',
-        name: 'lessons',
+      type: "input",
+      message:
+        "What leasons did you learn in the process of completing this project?",
+      name: "lessons",
     },
     {
-        type: 'input',
-        message: 'Please provide installation instructions.',
-        name: 'installation',
+      type: "input",
+      message: "Please provide installation instructions.",
+      name: "installation",
     },
     {
-        type: 'input',
-        message: 'Please provide instructions and examples for use.',
-        name: 'usage',
+      type: "input",
+      message: "Please provide instructions and examples for use.",
+      name: "usage",
     },
     {
-        type: 'confirm',
-        message: 'Do you have a screen shot of the project to provide?',
-        name: 'photo',
-        default: false
+      type: "list",
+      message: "What type of license would you like to attach to this project?",
+      choices: ['Public Domain Dedication and License (PDDL)', 'MIT License' , 'Apache 2.0 License' , 'Boost Software License 1.0'],
+      name: "license",
     },
     {
-        type: 'input',
-        message: 'Please provide the photo alt text.',
-        name: 'altText',
-        when: data => data.photo === true
+      type: "confirm",
+      message: "Do you have a screen shot of the project to provide?",
+      name: "photo",
+      default: false,
     },
     {
-        type: 'input',
-        message: 'Please provide the path for the photo.',
-        name: 'photoPath',
-        when: data => data.photo === true
+      type: "input",
+      message: "Please provide the photo alt text.",
+      name: "altText",
+      when: (data) => data.photo === true,
     },
     {
-        type: 'confirm',
-        message: 'Do you have another screen shot of the project to provide?',
-        name: 'additionalPhoto',
+      type: "input",
+      message: "Please provide the path for the photo.",
+      name: "photoPath",
+      when: (data) => data.photo === true,
     },
-    {
-        type: 'input',
-        message: 'Do you have another screen shot of the project to provide?',
-        name: 'additionalPhoto',
-    },
-])
-.then((responce) 
-    ,writeToFile(fileName, data))
+    
+    
 
-  
+  ])
+  .then((data) => {
+      inquirer.prompt([ {
+        type: "confirm",
+        message: "Do you have another screen shot of the project to provide?",
+        name: "additionalPhoto",
+      },
+      {
+        type: "input",
+        message: "Do you have another screen shot of the project to provide?",
+        name: "additionalPhoto",
+      },])
+    writeToFile(fileName, data);
+  });
 // TODO: Create a function to write README file
 
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, generateMarkdown(data), (err) =>
-  err ? console.error(err) : console.log('Success!')
-); };
-
+  fs.writeFile(fileName, generateMarkdown(data), (err) =>
+    err ? console.error(err) : console.log("Success!")
+  );
+}
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
 init();
-
-
-
-
